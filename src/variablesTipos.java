8eba92a17d57a82eadd2840f6e624b209e9d315e
Marcia Alegria <ytasegundo@gmail.com>
2024-03-12 18:37:14 -0300

    ejercicios clase 17 ejercicio2

---
+++ src/variablesTipos.java
@@ -0,0 +1,19 @@
+public class variablesTipos {
+    public static void main(String[] args) {
+        byte myFavoriteNumber = 14;
+        short myVariableShort = 1414;
+        int myVariableInt = 1414141414;
+        long myVariableLong = 1414141414L;
+
+        System.out.println("mi numero favorito variable byte " + myFavoriteNumber);
+        System.out.println("mi variable short " + myVariableShort);
+        System.out.println("mi variable int " + myVariableInt);
+        System.out.println("mi variable long " + myVariableLong);
+
+        float variableFloat = 3.40f;
+        double variableDouble = 52.6325d;
+        System.out.println("mi variable float " + variableFloat);
+        System.out.println("mi variable double " + variableDouble);
+
+    }
+}
