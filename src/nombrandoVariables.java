471b0a5c87e2eef68c2fbb36ac7fec7dc4361490
Marcia Alegria <ytasegundo@gmail.com>
2024-03-12 17:14:51 -0300

    clase 17 ejercicio 1

---
+++ src/nombrandoVariables.java
@@ -0,0 +1,10 @@
+public class nombrandoVariables {
+    public static <string> void main(String[] args) {
+        char identificador = 6;
+        String apellido = "Alegría";
+        System.out.println(identificador);
+        boolean estudiante = true;
+        System.out.println(estudiante);
+        System.out.println(apellido + " Marcia");
+    }
+}
