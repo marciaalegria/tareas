a17783ee488a754b8dc9c09648d6b3a6cf3281f6
Marcia Alegria <ytasegundo@gmail.com>
2024-04-10 17:33:06 -0400

    add file depurando

---
+++ src/Depurando.java
@@ -0,0 +1,12 @@
+public class Depurando {
+    public static void main(String[] args){
+        int x=0;
+        depurar(x);
+
+    }
+    public static void depurar(int x){
+        while(x < 1){
+            System.out.println("ciclo");
+        }
+    }
+}
